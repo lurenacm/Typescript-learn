99d6ce10937fc5911df88829dd4818048ef59f2c
临一一 <18320398437@139.com>
2021-01-17 17:50:42 +0800

    add Enum 枚举类型

---
+++ lesson10-Enum枚举类型/demo.ts
@@ -0,0 +1,37 @@
+enum Status  {
+    // OFFLINE = 1,
+    OFFLINE,
+    ONLINE,
+    OTHERS
+}
+
+// 对比常用的 JS 代码
+// const Status = {
+//     OFFLINE : 0,
+//     ONLINE: 1,
+//     OTHERS: 2
+// }
+
+function getStatus(status: Number) {
+    if(status === Status.ONLINE) {
+        return 'online'
+    }else if (status === Status.OFFLINE) {
+        return 'offline'
+    }else if(status === Status.OTHERS) {
+        return 'others'
+    }
+    return 'error'
+}
+
+const result = getStatus(Status.OFFLINE)
+console.log(result)
+
+// 下面代码直接打印出 enum 的下标值
+console.log(Status.OFFLINE)
+console.log(Status.ONLINE)
+console.log(Status.OTHERS)
+
+// 打印下标对应的属性
+console.log(Status[0])
+console.log(Status[1])
+console.log(Status[2])
