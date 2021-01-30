f193587c5a1a79eda30f4e5a6adbd34a12706c64
lurenacm <18320398437@139.com>
2021-01-30 19:45:23 +0800

    补充

--- lesson11-泛型/函数泛型/baseDemo.ts
+++ lesson11-泛型/函数泛型/baseDemo.ts
@@ -4,18 +4,27 @@ function id<T>(a: T, b: T, c: T): T {
 }
 
 // 不针对某一特定类型，可以是 number，string，Boolean，interface等类型，且使用的类型必须一致。
-interface type {
+interface Type {
     a: number
     b: number
     c: number
 }
-let t: type = {a: 2, b: 3, c: 4}
+let t: Type = {a: 2, b: 3, c: 4}
 
 id(1,2,3)
 id('1', '2', '3')
 id(false, true, false)
 id(t.a, t.b, t.c)
 
+// 接口 推荐写法，直接使用泛型参数 T 代表指定类型
+interface Uni<T> {
+    a: T
+    b: T
+    c: T
+}
+
+let uni: Uni<number> = {a: 2, b:3, c:4}
+id(uni.a, uni.b, uni.c)
 
 // ReadonlyArray 的只读泛型，
 let a: number[] = [1, 2, 3]
